8ee1a245e49f11d4bb561e1a8dde6b7d58fca3b3
Blinkner <167825090+Blinkner@users.noreply.github.com>
2024-04-30 15:46:59 -0300

    Add files via upload

---
+++ uaitec/lab08/footer.php
@@ -0,0 +1,12 @@
+		<div class="footer">
+			<div class="linha">
+				<footer>
+					<div class="coluna col12">
+						<span>&copy; 2016 - Gabriel Desenvolvimento Web</span>
+					</div>
+				</footer>
+			</div>
+		</div>
+		
+	</body>
+</html>
