85360003f08973bdf8eda5bf269363f613242a45
Blinkner <gabroc9@gmail.com>
2024-04-30 16:00:26 -0300

    Added from PC

---
+++ uaitec/sitefinal/footer.php
@@ -0,0 +1,12 @@
+		<div class="footer">
+			<div class="linha">
+				<footer>
+					<div class="coluna col12">
+						<span>&copy; 2016 - LGerken Desenvolvimento Web</span>
+					</div>
+				</footer>
+			</div>
+		</div>
+		
+	</body>
+</html>
