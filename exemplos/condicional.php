a3431d120b344217d9c53b95da9c497be90405e2
Blinkner <167825090+Blinkner@users.noreply.github.com>
2024-04-30 15:24:28 -0300

    Add files via upload

---
+++ exemplos/condicional.php
@@ -0,0 +1,13 @@
+<?php
+
+$A = 5;
+$B = 2;
+
+if($A % 2 == 1)
+{
+	echo "Número impar";
+}
+else
+{
+	echo "Número par";
+}
---
+++ exemplos/variaveis.php
@@ -0,0 +1,12 @@
+<?php
+
+$mensagem = "Hello world";
+
+echo $mensagem;
+
+$A = 2;
+$B = 3;
+
+$SOMA = $A + $B;
+
+print $SOMA;
