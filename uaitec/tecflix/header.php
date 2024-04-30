4cb7c2a08913afefd7a994a354e15f4035d1183c
Blinkner <gabroc9@gmail.com>
2024-04-30 16:00:57 -0300

    Added from PC

---
+++ uaitec/tecflix/header.php
@@ -0,0 +1,30 @@
+<?php
+	include("lib/conexao.php"); // Trazendo a Conexão com o Servidor
+?>
+<!doctype html>
+<html>
+	<head>
+		<title>Tecflix - Séries Originais</title>
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,500" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="css/estilo.css" />
+		<meta charset='utf-8' />
+	</head>
+	
+	<body>
+		<div class="header">
+			<div class="linha">
+				<header>
+					<div class="coluna col12">
+						<img class="logo" src="img/logo.png" />
+						<nav>
+							<ul class="menu inline sem-marcador">
+								<li><a href="index.php">home</a></li>
+								<li><a href="produtos.php">produtos</a></li>
+								<li><a href="sobre.php">sobre</a></li>
+							</ul>
+						</nav>
+					</div>
+				</header>
+			</div>
+		</div>
---
+++ uaitec/tecflix/sobre.php
@@ -0,0 +1,28 @@
+<?php
+	include("header.php"); // Trazendo o Cabeçalho
+?>		
+			
+		<div class="linha">
+			<section>
+			
+				<?php
+					include("sidebar.php"); // Trazendo a Barra Lateral
+				?>
+				
+				<div class="coluna col10">
+					<h1>Por trás dos códigos</h1>
+					<p>Esse website foi desenvolvido pelo grupo de alunos do curso de programador web
+					da UAITEC Patos de Minas, representado por Khrisley, Gabriel, Gusttavo e Christian.</p>
+					<p>Utilizando todo o conhecimento que adquirimos ao decorrer do curso em linguagens HTML, CSS, PHP e SQL,
+					procuramos apresentá-lo neste trabalho, tendo como objetivo a criação de um site
+					para venda de séries originais. E assim recolhida as mais conhecidas entre os integrantes.</p>
+					<p>Buscando um melhor desempenho, utilizamos páginas automatizadas, formulário para pedidos,
+					consultas no banco de dados para apresentação de resultados, tabela com informações, logotipo
+					original, etc.</p>
+				</div>
+			</section>
+		</div>
+		
+<?php
+	include("footer.php"); // Trazendo o Rodapé
+?>
