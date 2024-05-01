4dad711efd5893eb447305c8eb3a21ad2258d85b
Blinkner <167825090+Blinkner@users.noreply.github.com>
2024-05-01 13:46:39 -0300

    Update Pessoa.php

--- classe/Pessoa.php
+++ classe/Pessoa.php
@@ -2,20 +2,17 @@
 
 class Pessoa
 {
-	// public, private, protected.
-	protected $nome;
-	const ESPECIE = "Humana";	// Constante.
+    // public, private, protected.
+    protected $nome;
+    const ESPECIE = "Humana";	// Constante.
 
-	public function __construct($tmpNome)
-	{
-		$this->nome = $tmpNome;
-	}
-	public function setNome($novoNome)
-	{
-		$this->nome = $novoNome;
-	}
-	public function getNome()
-	{
-		return $this->nome;
-	}
+    public function __construct($tmpNome) {
+        $this->nome = $tmpNome;
+    }
+    public function setNome($novoNome) {
+        $this->nome = $novoNome;
+    }
+    public function getNome() {
+        return $this->nome;
+    }
 }
