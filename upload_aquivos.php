fc68f3f9e452e9024ffae282b05ae2dfd19e47c2
Blinkner <167825090+Blinkner@users.noreply.github.com>
2024-05-27 11:16:29 -0300

    Update upload_aquivos.php

--- upload_aquivos.php
+++ upload_aquivos.php
@@ -1,11 +1,11 @@
-// Formulário para upload de arquivo
+// Formulário HTML para upload de arquivo
 <form enctype="multipart/form-data" action="" method="post">
     <input type="hidden" name="MAX_FILE_SIZE" value="9000000" />
     <input type="file" name="input_file" accept="image/jpg, image/jpeg, image/png, image/webp">
     <input type="submit" name="upload" value="upload">
 </form>
 
-// Código para upload no servidor
+// Código PHP para upload no servidor
 <?php
         $caminho = ""; // Caminho completo onde o arquivo será salvo dentro do servidor
         $extensao = explode('/', $_FILES['input_file']['type']); // Separação do tipo e da extensão do arquivo
