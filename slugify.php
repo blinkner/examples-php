ccd35417e64d389388bf080e0876cf62991c8d26
Blinkner <167825090+Blinkner@users.noreply.github.com>
2024-05-28 15:06:07 -0300

    Update slugify.php

--- slugify.php
+++ slugify.php
@@ -1,12 +1,14 @@
 function slugify($nome) {
     $slug = $nome;
-    $slug = str_replace("-", "", $slug);
+    $slug = str_replace("- ", "", $slug);
     $slug = str_replace(":", "", $slug);
     $slug = str_replace(" ", "-", $slug);
     $slug = str_replace("á", "a", $slug);
+    $slug = str_replace("à", "a", $slug);
     $slug = str_replace("ã", "a", $slug);
     $slug = str_replace("é", "e", $slug);
     $slug = str_replace("õ", "o", $slug);
+    $slug = str_replace("í", "i", $slug);
     $slug = strtolower($slug);
 
     return $slug;
