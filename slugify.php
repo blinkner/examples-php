439a19f17cf862ded32a3776e38cf4326780d371
Blinkner <167825090+Blinkner@users.noreply.github.com>
2024-05-28 15:06:57 -0300

    Update slugify.php

--- slugify.php
+++ slugify.php
@@ -7,8 +7,9 @@ function slugify($nome) {
     $slug = str_replace("à", "a", $slug);
     $slug = str_replace("ã", "a", $slug);
     $slug = str_replace("é", "e", $slug);
-    $slug = str_replace("õ", "o", $slug);
     $slug = str_replace("í", "i", $slug);
+    $slug = str_replace("õ", "o", $slug);
+    $slug = str_replace("ú", "u", $slug);
     $slug = strtolower($slug);
 
     return $slug;
