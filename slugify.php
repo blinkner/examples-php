76e95dab9793455f5d8bc187976bd227a977b280
Blinkner <167825090+Blinkner@users.noreply.github.com>
2024-05-28 15:05:04 -0300

    Create slugify.php

---
+++ slugify.php
@@ -0,0 +1,13 @@
+function slugify($nome) {
+    $slug = $nome;
+    $slug = str_replace("-", "", $slug);
+    $slug = str_replace(":", "", $slug);
+    $slug = str_replace(" ", "-", $slug);
+    $slug = str_replace("á", "a", $slug);
+    $slug = str_replace("ã", "a", $slug);
+    $slug = str_replace("é", "e", $slug);
+    $slug = str_replace("õ", "o", $slug);
+    $slug = strtolower($slug);
+
+    return $slug;
+}
