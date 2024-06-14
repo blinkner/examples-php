b4cc926545facc2ec5b4f21b93f6379cac500a3d
Blinkner <167825090+Blinkner@users.noreply.github.com>
2024-06-13 22:46:01 -0300

    Create gerar_senha_aleatoria.php

---
+++ gerar_senha_aleatoria.php
@@ -0,0 +1,14 @@
+<?php
+// Gerar senha aleatória com 8 caracteres
+$maiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$minusculas = 'abcdefghijklmnopqrstuvwxyz';
+$numeros = '0123456789';
+$simbolos = '!@#$%&*()_+=';
+$tamanho = 8;
+
+$senha = str_shuffle($maiusculas);
+$senha .= str_shuffle($minusculas);
+$senha .= str_shuffle($numeros);
+$senha .= str_shuffle($simbolos);
+
+$senha = substr(str_shuffle($senha), 0, $tamanho);
