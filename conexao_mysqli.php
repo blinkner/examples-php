c206251f384c439dbe1cec601add72a6d3e5f907
Blinkner <167825090+Blinkner@users.noreply.github.com>
2024-09-21 15:07:30 -0300

    Update conexao_mysqli.php

--- conexao_mysqli.php
+++ conexao_mysqli.php
@@ -1,19 +1,19 @@
 <?php
-    // use mysqli;
+// use mysqli;
 
-    define("SERVER_DB", ""); // localhost
-    define("USER_DB", ""); // Usuário do banco de dados
-    define("PASS_DB", ""); // Senha do banco de dados
-    define("DATABASE_DB", ""); // Nome do banco de dados
+define("SERVER_DB", ""); // localhost
+define("USER_DB", ""); // Usuário do banco de dados
+define("PASS_DB", ""); // Senha do banco de dados
+define("DATABASE_DB", ""); // Nome do banco de dados
 
-    // Mensagens de erro
-    $msg[0] = "Conexão com o banco falhou!";
+// Mensagens de erro
+$msg[0] = "Conexão com o banco falhou!";
 
-    // Fazendo a conexão com o servidor MySQL
-    $conexao = new mysqli(SERVER_DB, USER_DB, PASS_DB, DATABASE_DB);
-    if ($conexao->connect_errno > 0) {
-        trigger_error($msg[0]);
-    }
+// Fazendo a conexão com o servidor MySQL
+$conexao = new mysqli(SERVER_DB, USER_DB, PASS_DB, DATABASE_DB);
+if ($conexao->connect_errno > 0) {
+    trigger_error($msg[0]);
+}
 
-    // Setando charset do banco
-    $conexao->set_charset("utf8");
+// Setando charset do banco
+$conexao->set_charset("utf8");
