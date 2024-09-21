08f212f02e718d833f2e1d606da3d1b0a5c66229
Blinkner <167825090+Blinkner@users.noreply.github.com>
2024-09-21 15:09:17 -0300

    Update sql_sheet.php

--- sql_sheet.php
+++ sql_sheet.php
@@ -1,8 +1,8 @@
 <?php
-	SELECT 		// coluna escolhida Ex: nome, email
-	FROM 		// tabela escolhida Ex: produtos
-	WHERE		// condição ou coluna para a pesquisa do LIKE
-		LIKE	// procura específica Ex: '%the%'
-	ORDER BY 	// ordena uma coluna em ordem crescente ou decrescente Ex: nome ASC, email DESC
-	GROUP BY 	// agrupa uma coluna em ordem crescente ou decrescente Ex: nome ASC, email DESC
+SELECT 		// coluna escolhida Ex: nome, email
+FROM 		// tabela escolhida Ex: produtos
+WHERE		// condição ou coluna para a pesquisa do LIKE
+ LIKE	// procura específica Ex: '%the%'
+ORDER BY 	// ordena uma coluna em ordem crescente ou decrescente Ex: nome ASC, email DESC
+GROUP BY 	// agrupa uma coluna em ordem crescente ou decrescente Ex: nome ASC, email DESC
 ?>
